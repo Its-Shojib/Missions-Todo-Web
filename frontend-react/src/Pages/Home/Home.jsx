362dda9e47d8234b64fcb2af4ccaaf4f71f3af17
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-20 17:04:24 +0600

    Task SHowed to home page

--- frontend-react/src/Pages/Home/Home.jsx
+++ frontend-react/src/Pages/Home/Home.jsx
@@ -3,21 +3,22 @@ import UseLoadMyTasks from "../../Hooks/useLoadMyTasks";
 
 
 const Home = () => {
-    let [myTask, isPending, refetch] = UseLoadMyTasks();
+    let [myTask, isPending] = UseLoadMyTasks();
     console.log(myTask)
 
     return (
-        <div>
+        <div className="bg-green-200 min-h-screen">
             <CreateTask />
-            <div>
+            <div className="my-20 max-w-screen-sm mx-auto ">
                 {isPending && <p>Loading...</p>}
                 {!isPending && myTask?.map((task) => (
                     <div key={task.id}>
-                        <h3>{task.title}</h3>
-                        <p>{task.completed}</p>
+                        <div className="flex items-center gap-2 bg-white my-3">
+                            <input type="checkbox" checked={task.completed} />
+                            <h3 className={`text-lg ${task?.completed? "line-through ":""}`}>{task.title}</h3>
+                        </div>
                     </div>
                 ))}
-                <button onClick={refetch}>Refresh</button>
             </div>
         </div>
     );
