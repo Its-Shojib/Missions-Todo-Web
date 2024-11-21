2093ffad4223589446255a58ce8ad972a6a48805
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-21 09:16:10 +0600

    Layout Changed

--- frontend-react/src/Layout/Layout.jsx
+++ frontend-react/src/Layout/Layout.jsx
@@ -4,12 +4,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <div className="max-w-screen-2xl mx-auto px-16 md:px-4">
-            <h1 className="text-center text-2xl font-semibold text-rose-900">Missions | Todo App</h1>
-
             <Outlet></Outlet>
-            
-
-    
         </div>
     );
 };
